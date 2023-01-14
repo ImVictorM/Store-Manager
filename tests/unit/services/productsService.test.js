8793e1ae997985e17cc9cce97392854c4e812df2
Victor Mendes <victorutdsim7@gmail.com>
2023-01-14 12:39:39 -0300

    [REQ 2 - incompleto]: fix products service tests

--- tests/unit/services/productsService.test.js
+++ tests/unit/services/productsService.test.js
@@ -21,16 +21,19 @@ describe('Testing products service', function () {
   });
 
   describe('Getting a product by id', function () {
-    it('Can get a product by id receiving a valid name', async function () {
+    it('Can get a product by id receiving a valid id', async function () {
       const idOneProduct = allProductsFromDB[0];
       sinon.stub(productsModel, 'findById').resolves(idOneProduct);
 
       const response = await productsService.getById(1);
 
-      expect(response).to.be.deep.equal(idOneProduct);
+      expect(response).to.be.deep.equal({
+        type: null,
+        message: idOneProduct,
+      });
     });
 
-    it('Retuns an error when receiving an invalid name', async function () {
+    it('Retuns an error when receiving an invalid id', async function () {
       const invalidId = 999;
       sinon.stub(productsModel, 'findById').resolves(undefined);
 
