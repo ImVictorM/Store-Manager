4b2b0092f8aac47fa9d943ee1e45b1ddd01f55b5
Victor Mendes <victorutdsim7@gmail.com>
2023-01-14 12:37:09 -0300

    [REQ 2 - incompleto]: mocks de products

---
+++ tests/unit/mocks/productsMock.js
@@ -0,0 +1,18 @@
+const allProductsFromDB = [
+  {
+    id: 1,
+    name: 'product 1'
+  },
+  {
+    id: 2,
+    name: 'product 2'
+  },
+  {
+    id: 3,
+    name: 'product 3'
+  }
+]
+
+module.exports = {
+  allProductsFromDB,
+}
