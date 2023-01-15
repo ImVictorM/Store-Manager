9339f6b42040392dfddd0cfe268a3b436c6154d1
victor <victorutdsim7@gmail.com>
2023-01-14 22:28:38 -0300

    [Req 3 - incompleto] modelo para criar novo produto na rota /products

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -12,7 +12,18 @@ async function findById(id) {
   return product[0];
 }
 
+async function insertNew(product) {
+  const colums = Object.keys(product);
+  const values = Object.values(product);
+  const placeholders = Array(colums.length).fill('?').join(', ');
+  const query = `INSERT INTO StoreManager.products (${placeholders}) VALUES (${placeholders})`;
+  const [{ insertId }] = await connection.execute(query, [...colums, ...values]);
+  const createdProduct = await findById(insertId);
+  return createdProduct;
+}
+
 module.exports = {
   findAll,
   findById,
+  insertNew,
 };
